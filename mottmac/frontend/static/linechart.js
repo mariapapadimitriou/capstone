5973a942505fbb55bc160d075d044e526bc087f9
mariaapapadimitriou <maria.papadimitriou@mail.utoronto.ca>
2022-01-03 21:25:16 -0500

    multi-obj chart

--- mottmac/frontend/static/linechart.js
+++ mottmac/frontend/static/linechart.js
@@ -1,70 +1,61 @@
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
-var myChart;
-
 function getMultiObjective(plot_colours, plot_labels, plot_data) {
 
-  var multi_data = []
+  console.log(plot_data)
+  
+  data = []
 
-  for (let i = 0; i < plot_colours.length; i++) {
+  for (let i=0; i < plot_labels.length; i++) {
 
-    point = {
-        data: plot_data[i],
-        label: plot_labels[i],
-        borderColor:  plot_colours[i],
-        backgroundColor: plot_colours[i],
-        fill: false
+    data.push(
+      {
+        x: ["Cost", "Ridership", "Safety"],
+        y: plot_data[i],
+        error_y: {
+          type: 'data',
+          symmetric: false,
+          array: [1, 2, 1],
+          arrayminus: [0.2, 0.4, 1, 0.2],
+          visible: true
+        },
+        type: 'line',
+        marker: {
+          color: plot_colours[i],
+          size: 10
+        },
+        hoverinfo: "skip", // show only value, no serie name
+      }
+    )
+  }
+
+  var layout = {
+        font: {
+            family: 'Nunito',
+            size: 12,
+            color: '#7f7f7f'
+        },
+        hoverlabel: {
+            font: {
+                family: 'Nunito',
+            }
+        },
+        margin: {
+            autoexpand: true,
+            b: 30,
+            t: 10,
+            l: 30,
+            r: 0
+        },
+        xaxis : {
+            zeroline:false, 
+        },
+        yaxis : {
+            zeroline:false, 
+            hoverformat: '.1f', // float precision
+        },
+        showlegend: false
     }
-    multi_data.push(point)
-}
-if (myChart) myChart.destroy();
-
-myChart = new Chart(document.getElementById("myChart"), {
-    type: 'line',
-    data: {
-      labels: ["Cost","Ridership","Safety"],
-      datasets: multi_data
-    },
-    options: {
-      animation: {
-        duration: 0
-      },
-      elements: {
-        line: {
-            tension: 0
-        }
-      },
-      title: {
-        display: false,
-        text: 'Route Comparison'
-      },
-      legend: {
-        display: false
-      },
-      scales: {
-        xAxes: [{
-          gridLines: {
-            display: false,
-            drawBorder: false
-          },
-          ticks: {
-            maxTicksLimit: 7
-          }
-        }],
-        yAxes: [{
-          ticks: {
-            display: false
-          },
-          gridLines: {
-            color: "rgb(234, 236, 244)",
-            zeroLineColor: "rgb(234, 236, 244)",
-            drawBorder: false,
-            borderDash: [2],
-            zeroLineBorderDash: [2]
-          }
-        }],
-      },
-    }
-  });
+  return Plotly.newPlot('myChart', data, layout, {displayModeBar: false});
 }
