07077f39bad450f345e6257fbccdbe89d6b0e4b1
mariaapapadimitriou <maria.papadimitriou@mail.utoronto.ca>
2022-01-05 00:19:52 -0500

    lane_closure

--- mottmac/frontend/static/trafficGraph.js
+++ mottmac/frontend/static/trafficGraph.js
@@ -1,16 +1,14 @@
 function getTrafficPlot(plot_colours, plot_labels, plot_data) {
 
-    var return_string = "<div style='display: flex; justify-content: space-evenly;'>"
-
+    var return_string = "<div style='display: flex; justify-content: space-around;'>"
+    
     for (let i = 0; i < plot_colours.length; i++) {
 
-        return_string += "<span style='color:" + plot_colours[i] + ";'>"
-
         if (plot_data[i] == 1) {
-            return_string += "<b>" + plot_labels[i] + "</b> &nbsp;&nbsp;&nbsp;<span style='color: #ff1818;'><i class='fas fa-minus-circle'></i></span>"
+            return_string += "<span style='color: #ff1818; font-size: 50px;'><i class='fas fa-minus-circle'></i></span>"
         }
         else {
-            return_string += "<b>" + plot_labels[i] + "</b> &nbsp;&nbsp;&nbsp;<span style='color: #1cc88a;'><i class='fas fa-check-circle'></i></span>"
+            return_string += "<span style='color: #1cc88a; font-size: 50px;'><i class='fas fa-check-circle'></i></span>"
         }
 
         return_string += "</span>"
