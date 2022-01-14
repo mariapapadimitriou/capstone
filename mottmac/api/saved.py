b7571696c9a184e5b1b8e7d7bed0732ddfac780b
daniellepage <danielle.page@mail.utoronto.ca>
2022-01-13 22:15:28 -0500

    add coordinates field to route, fix route data types

--- mottmac/api/saved.py
+++ mottmac/api/saved.py
@@ -2,10 +2,12 @@ from django.db import models
 
 import sqlite3
 import pandas as pd
+import json
 
 OVERRIDE_COLUMNS = ['override_name', 'sharrows_cost_min', 'sharrows_cost_max', 'striped_cost_min', 'striped_cost_max', 'protected_cost_min', 'protected_cost_max', 
                     'bicycle_commuters_min', 'bicycle_commuters_max', 'new_riders_min', 'new_riders_max', 'emissions_per_km_min', 'emissions_per_km_max']
 ROUTE_COLUMNS = ['route_name', 'start_coordinates', 'end_coordinates']
+# ROUTE_COLUMNS = ['route_name', 'coordinates']
 
 OVERRIDE_PLACEHOLDERS = ("?,"*len(OVERRIDE_COLUMNS))[:-1]
 ROUTE_PLACEHOLDERS = ("?,"*len(ROUTE_COLUMNS))[:-1]
@@ -28,10 +30,15 @@ ROUTE_PLACEHOLDERS = ("?,"*len(ROUTE_COLUMNS))[:-1]
 #     # 'user': 'NULL'
 # }
 
-# route_dict = {
-#     'route_name': 'test_route'
-#     'start_coordinates': 
-#     'end_coordinates': 
+# route_dict1 = {
+#     'route_name': 'test_route',
+#     'start_coordinates': ['-79.407199', '43.675222'],
+#     'end_coordinates': ['-79.403963', '43.66695']
+# }
+
+# route_dict2 = {
+#     'route_name': 'test_route',
+#     'coordinates': [['-79.407199', '43.675222'], ['-79.403963', '43.66695']]
 # }
 
 def getConnCurs():
@@ -42,12 +49,42 @@ def getConnCurs():
     return conn, curs
     
 
-def getAllNames(name_type):
+### VIEW
+
+def getAllSaved(saved_type, user = None):
     # name_type is either "override" or "route"
+
+    if name_type == 'override':
+        columns = ','.join(OVERRIDE_COLUMNS)
+    elif name_type == 'route':
+        columns = ','.join(ROUTE_COLUMNS)
+    else:
+        return None
+    
+    sqlQuery = "SELECT {columns} FROM saved_{saved_type}s".format(columns = columns, saved_type = saved_type) 
+    
+    if user is not None:
+        sqlQuery = sqlQuery + " where user = '{}'".format(user)
     
     conn, curs = getConnCurs()
 
-    curs.execute("SELECT {0}_name FROM saved_{0}s".format(name_type))    
+    vals = pd.read_sql_query(sqlQuery, conn)
+
+    # curs.execute(sqlQuery)    
+    # vals = curs.fetchall()
+    
+    curs.close()
+    conn.close()
+
+    return vals
+
+
+def getAllNames(saved_type):
+    # saved_type is either "override" or "route"
+    
+    conn, curs = getConnCurs()
+
+    curs.execute("SELECT {0}_name FROM saved_{0}s".format(saved_type))    
     names = curs.fetchall()
     
     curs.close()
@@ -91,9 +128,10 @@ def saveOverride(override_dict):
     
     return status
 
-    def saveRoute(route_dict):
+def saveRoute(route_dict):
     
     route_name = route_dict['route_name']
+    coordinates = str(route_dict['coordinates'])
         
     if route_name in getAllNames("route"):
         status = 1
@@ -101,7 +139,7 @@ def saveOverride(override_dict):
         return status_message, status
     
     columns = ",".join(ROUTE_COLUMNS)
-    route_vals = tuple(route_dict.values())
+    route_vals = tuple([route_name, coordinates])
     sqlQuery = "INSERT INTO saved_routes ({0}) VALUES ({1})".format(columns, ROUTE_PLACEHOLDERS)
 
     conn, curs = getConnCurs()
@@ -120,6 +158,70 @@ def saveOverride(override_dict):
         conn.close()
     
     return status, status_message
+
+
+### EDIT
+
+def editOverride(override_dict):
+    
+    override_name = override_dict['override_name']
+        
+    if override_name not in getAllNames("override"):
+        status, status_message = 1, "Error: This override does not exist."
+        return status, status_message
+    
+    sqlQuery = "UPDATE saved_overrides SET "
+    for col in OVERRIDE_COLUMNS[1:]:
+        sqlQuery = sqlQuery + "{col} = {value}, ".format(col=col, value=override_dict[col])
+    
+    sqlQuery = sqlQuery + "update_time = CURRENT_TIMESTAMP WHERE override_name = '{}'".format(override_name)
+    
+    conn, curs = getConnCurs()
+
+    try:
+        curs.execute(sqlQuery)
+        conn.commit()
+        status, status_message = 0, override_name + " has been successfully updated."
+    except:
+        status, status_message = 1,  "Error - Please try again."
+        
+    finally:
+        curs.close()
+        conn.close()
+        
+    return status, status_message
+
+def editRoute(route_dict):
+    
+    route_name = route_dict['route_name']
+    coordinates = str(route_dict['coordinates']).replace("'", "''")
+        
+    if route_name not in getAllNames("route"):
+        status, status_message = 1, "Error: This route does not exist."
+        return status, status_message
+    
+    sqlQuery = "UPDATE saved_routes SET coordinates = '{0}', update_time = CURRENT_TIMESTAMP WHERE route_name = '{1}'".format(coordinates, route_name)
+    
+    # for col in ROUTE_COLUMNS[1:]:
+    #     sqlQuery = sqlQuery + "{col} = {value}, ".format(col=col, value=route_dict[col])
+    
+    # sqlQuery = sqlQuery + "update_time = CURRENT_TIMESTAMP WHERE route_name = '{}'".format(route_name)
+    
+    conn, curs = getConnCurs()
+
+    try:
+        curs.execute(sqlQuery)
+        conn.commit()
+        status, status_message = 0, route_name + " has been successfully updated."
+    except:
+        status, status_message = 1,  "Error - Please try again."
+        
+    finally:
+        curs.close()
+        conn.close()
+        
+    return status, status_message
+
 
 ### RETRIEVE VALUES
 
@@ -158,65 +260,8 @@ def getSavedRoute(route_name):
     
     for i, col in enumerate(ROUTE_COLUMNS):
         route[col] = route_vals[i]
+
+    route['coordinates'] = json.loads(route['coordinates'].replace('\'', '"'))
     
     return route
 
-### EDIT
-
-def editOverride(override_dict):
-    
-    override_name = override_dict['override_name']
-        
-    if override_name not in getAllNames("override"):
-        status, status_message = 1, "Error: This override does not exist."
-        return status, status_message
-    
-    sqlQuery = "UPDATE saved_overrides SET "
-    for col in OVERRIDE_COLUMNS[1:]:
-        sqlQuery = sqlQuery + "{col} = {value}, ".format(col=col, value=override_dict[col])
-    
-    sqlQuery = sqlQuery + "update_time = CURRENT_TIMESTAMP WHERE override_name = '{}'".format(override_name)
-    
-    conn, curs = getConnCurs()
-
-    try:
-        curs.execute(sqlQuery)
-        conn.commit()
-        status, status_message = 0, override_name + " has been successfully updated."
-    except:
-        status, status_message = 1,  "Error - Please try again."
-        
-    finally:
-        curs.close()
-        conn.close()
-        
-    return status, status_message
-
-    def editRoute(route_dict):
-    
-    route_name = route_dict['route_name']
-        
-    if route_name not in getAllNames("route"):
-        status, status_message = 1, "Error: This route does not exist."
-        return status, status_message
-    
-    sqlQuery = "UPDATE saved_routes SET "
-    for col in ROUTE_COLUMNS[1:]:
-        sqlQuery = sqlQuery + "{col} = {value}, ".format(col=col, value=route_dict[col])
-    
-    sqlQuery = sqlQuery + "update_time = CURRENT_TIMESTAMP WHERE route_name = '{}'".format(route_name)
-    
-    conn, curs = getConnCurs()
-
-    try:
-        curs.execute(sqlQuery)
-        conn.commit()
-        status, status_message = 0, route_name + " has been successfully updated."
-    except:
-        status, status_message = 1,  "Error - Please try again."
-        
-    finally:
-        curs.close()
-        conn.close()
-        
-    return status, status_message
