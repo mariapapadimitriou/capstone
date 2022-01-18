f60d485113fb8c340744397b6a6d4cf87c675102
daniellepage <danielle.page@mail.utoronto.ca>
2022-01-18 13:08:02 -0500

    delete unneeded python files

--- mottmac/api/apps.py
+++ mottmac/api/apps.py
@@ -1,6 +1,7 @@
-from django.apps import AppConfig
+# Can probs delete this but leaving commented for a bit to make sure nothing breaks
+# from django.apps import AppConfig
 
 
-class ApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api'
+# class ApiConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'api'
--- mottmac/frontend/apps.py
+++ mottmac/frontend/apps.py
@@ -1,6 +1,7 @@
-from django.apps import AppConfig
+# Can probs delete this but leaving commented for a bit to make sure nothing breaks
+# from django.apps import AppConfig
 
 
-class FrontendConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'frontend'
+# class FrontendConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'frontend'
